ce5f8a2ede765bb13456735ffee561b4a42b366c
gaimerI17 <gaimeri255@gmail.com>
2025-02-25 15:35:35 +0200

    Delete data/examples directory
    I had no idea I could delete an entire directory at once! This is huge!

--- data/examples/example.clj
+++
@@ -1,7 +0,0 @@
-(defn factorial [n]
-  (if (<= n 1)
-    1
-    (* n (factorial (dec n)))))
-
-;; Example usage:
-(println (factorial 5))  ;; Output: 120
--- data/examples/example.fs
+++
@@ -1,8 +0,0 @@
-let rec factorial n =
-    if n <= 1 then 1
-    else n * factorial (n - 1)
-
-// Example usage
-let number = 5
-let result = factorial number
-printfn "Factorial of %d is %d" number result
