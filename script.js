b233d2a6fc2cbba2d1364a2d317bd82005a840be
gaimerI17 <gaimeri255@gmail.com>
2025-10-14 10:39:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.domElement.id = 'three';
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
